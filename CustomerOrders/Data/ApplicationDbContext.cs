6ab3a0fc7a9aded6ddce06802d4fa21278fbdea2
khurram.rafi <khurram.rafi@10pearls.local>
2019-03-27 20:24:20 +0500

    First version of app

---
+++ CustomerOrders/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CustomerOrders.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerOrders.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //this.Database.Migrate();
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
